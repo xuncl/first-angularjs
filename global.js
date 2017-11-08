a2291f99135ece2e7fc9c5a41af2919cf765c96a
xuncl <xuncl@126.com>
2017-11-08 18:34:09 +0800

    test grunt, concat+jshint+uglify+connect+watch :+1:

---
+++ global.js
@@ -0,0 +1,91 @@
+// 需要jquery
+
+function ck_log2(operation,param){
+    // 参数可以不写
+
+    if (!operation) operation = "browse";
+    var result="1";
+    var page = window.location.pathname;
+    if (!param) param = window.location.search;
+    var title = $("title").html();
+    if(!title) {
+        title = "空";
+    }else if(title.indexOf("weixin")==0){
+        title = "微信跳转";
+    }
+    if(operation=='dial'){
+        var array = param.split(",");
+        if (array[0]) param = array[0];
+        if (array[1]) result = array[1];
+    }
+    var subData = {
+        "page":page,
+        "param":param,
+        "title":title,
+        "oper":operation,
+        "result":result
+    };
+    //var url = "http://localhost/superkzy/index.php/Home/Log/fetch";
+    var url = "http://www.xuncl.com/index.php/Home/Log/fetch";
+    $.ajax({
+        type:"post",
+        url:url,
+        data:subData,
+        success:function(data){
+        },
+        error:function(XMLHttpRequest,textStatus,errorThrown){
+        }
+    });
+}
+
+$(document).ready(function(){
+    ck_log();
+});
+
+function testGrunt(){
+    alert("hello world2!");
+
+}
+// 需要jquery
+
+function ck_log2(operation,param){
+    // 参数可以不写
+
+    if (!operation) operation = "browse";
+    var result="1";
+    var page = window.location.pathname;
+    if (!param) param = window.location.search;
+    var title = $("title").html();
+    if(!title) {
+        title = "空";
+    }else if(title.indexOf("weixin")==0){
+        title = "微信跳转";
+    }
+    if(operation=='dial'){
+        var array = param.split(",");
+        if (array[0]) param = array[0];
+        if (array[1]) result = array[1];
+    }
+    var subData = {
+        "page":page,
+        "param":param,
+        "title":title,
+        "oper":operation,
+        "result":result
+    };
+    //var url = "http://localhost/superkzy/index.php/Home/Log/fetch";
+    var url = "http://www.xuncl.com/index.php/Home/Log/fetch";
+    $.ajax({
+        type:"post",
+        url:url,
+        data:subData,
+        success:function(data){
+        },
+        error:function(XMLHttpRequest,textStatus,errorThrown){
+        }
+    });
+}
+
+$(document).ready(function(){
+    ck_log();
+});
